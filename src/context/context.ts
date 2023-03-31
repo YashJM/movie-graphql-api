bf28e85a100b025e0e388e9b5033de8a39e6d1be
Yash Mistry <mistryyash100@gmail.com>
2023-03-31 15:53:46 -0400

    Created authentication and authorization functionality (#4)

--- src/context/context.ts
+++ src/context/context.ts
@@ -1,29 +1,47 @@
 import { PrismaClient } from '@prisma/client';
+import { GraphQLError } from 'graphql';
 import { decodeAuthHeader, AuthTokenPayload } from '../utils/auth';
 
 export interface Context {
   prisma: PrismaClient;
-  user: AuthTokenPayload | null;
+  user?: AuthTokenPayload | null;
 }
 
 const prisma = new PrismaClient();
 
 export const createContext = async ({ req }: any): Promise<Context> => {
-  const user = req && req.headers.authorization
-    ? decodeAuthHeader(req.headers.authorization)
-    : null;
+  let user: AuthTokenPayload | null = null;
 
-  if (req.body.operationName === 'signup' ||
-    req.body.operationName === 'login'
-  ) {
-    return {
-      prisma,
-      user: user
-    };
+  if (req && req.headers.authorization) {
+    try {
+      user = decodeAuthHeader(req.headers.authorization);
+    } catch (err) {
+      throw new GraphQLError('Invalid or expired authentication token.', {
+        extensions: {
+          code: 'UNAUTHENTICATED',
+        },
+      });
+    }
   }
 
-  return {
-    prisma,
-    user: user
+  // Allow anonymous access to sign up and login
+  if (req && req.body && req.body.operationName) {
+    if (['signup', 'login'].includes(req.body.operationName.toLowerCase())) {
+      return { prisma, user };
+    }
   }
+
+  // Check if user is authenticated for all other mutations
+  if (req && req.body && req.body.operationName) {
+    if (req.body.operationName.toLowerCase() === 'mutation' && !user) {
+      throw new GraphQLError('User is not authenticated', {
+        extensions: {
+          code: 'UNAUTHENTICATED',
+          http: { status: 401 },
+        },
+      });
+    }
+  }
+
+  return { prisma, user };
 };
